3a28eeae0ec2ab8c3f705e95c3d21de6e829f55c
Syarief A <syariefarifin107@gmail.com>
2019-08-01 22:05:23 +0700

    hasilnya keluar, cuma di VS ngeluarin Error, dear trainer. Tolong diinfokan terkait permasalahannya ke saya. Karena overall hasilnya sesuai, saya kumpulin aja dulu

---
+++ exercise-2.js
@@ -0,0 +1,18 @@
+//input "hello world!"
+
+//output
+
+// "!dlrow olleh"
+
+function balikString(hello) {
+    var balikHello = hello.split('');
+    var hello = "";
+    for (var b = balikHello.length-1; b >= 0; b--){
+        for (var b = balikHello.length-1; b >= 0; b--){        
+        hello = hello + balikHello[b];
+    } 
+}
+    return hello;
+}
+
+console.log(balikString('hello world!'));
