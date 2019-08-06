ee28edaf7912f07fd0ae0c96bf0c74a99ec551ff
Syarief A <syariefarifin107@gmail.com>
2019-08-06 17:15:26 +0700

    revisi

--- exercise-11.js
+++ exercise-11.js
@@ -1,15 +1,16 @@
 function tentukanDeretAritmatika(arr) {
     var aritAwal = arr[1] - arr[0];
-    var aritAkhir = 0;
+    var aritAngka = 0;
     for (i = arr.length-1; i > 0; i--){
-        aritAkhir = aritAkhir + (arr[i]-arr[i-1]);
-        if (aritAwal === aritAkhir) {
-            return true;
-        } else {
+        aritAngka = aritAngka + (arr[i]-arr[i-1]);
+        if (aritAwal !== aritAngka) {
             return false;
         }
+    aritAngka = 0;   
     }
+    return true;
 }
+
   
   // TEST CASES
   console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
@@ -17,3 +18,6 @@ function tentukanDeretAritmatika(arr) {
   console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
   console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
   console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+  console.log(tentukanDeretAritmatika([2, 4, 6, 12, 14]));
+  console.log(tentukanDeretAritmatika([2, 5, 6, 12, 15]));
+  
